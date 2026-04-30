79bd4dd66d744ef520095d8ae2ed00242d1cbd39
rcardiaz <reinadm001@gamil.com>
2026-04-29 18:46:48 -0600

    Estructuras repetitivas

---
+++ estructuras_repetitivas.js
@@ -0,0 +1,18 @@
+//Se usan para repetir bloques de código (bucles-la forma en que repetimos una acción)
+//FOR-WHILE-DO WHILE
+//Imprimiendo los números del 1 al 10
+ 
+console.log("------------------------uso de for------")
+
+for (let i = 1; i <=10; i++){
+console.log(i)
+}
+ 
+console.log("------------------------uso de while------")
+//while (condition) {
+//imprimiendo los numeros del 1 al 10
+let j=1;
+while(j<=10){
+    console.log(j)
+    j++
+} 
